a302c79945d596cfd9a084905c0315ab23eb290a
alfonsoa <alfonsoa@microsoft.com>
2024-08-14 16:56:46 +0200

    fix

--- my-app/src/app/main/page.tsx
+++ my-app/src/app/main/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
+
 import { useRouter } from 'next/router';
-
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p> welcome</p>
+      <p>Hello</p>
+      
     </main>
   );
 }
